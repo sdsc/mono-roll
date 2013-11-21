cebd06298fea71e86228c9f10bccc490407f52e3
Trevor Cooper <tcooper@sdsc.edu>
2013-11-21 10:31:11 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/mono/version.mk
+++ src/mono/version.mk
@@ -1,3 +1,16 @@
-NAME    = mono
-VERSION = 2.10.8
-RELEASE = 1
+NAME               = mono
+VERSION            = 2.10.8
+RELEASE            = 1
+PKGROOT            = /opt/mono
+
+SRC_SUBDIR         = mono
+
+SOURCE_NAME        = $(NAME)
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tar.gz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_GZ_PKGS        = $(SOURCE_PKG)
+
+==========
