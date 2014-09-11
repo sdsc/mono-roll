84c0c9365b74d1184d718a265a374a2a0e4d65f1
Jim Hayes <jhayes@sdsc.edu>
2014-09-10 23:00:49 -0700

    Normalize version.mk contents.

--- src/mono/version.mk
+++ src/mono/version.mk
@@ -1,16 +1,16 @@
-NAME               = mono
-VERSION            = 3.2.6
-RELEASE            = 0
-PKGROOT            = /opt/mono
+NAME           = mono
+VERSION        = 3.2.6
+RELEASE        = 0
+PKGROOT        = /opt/mono
 
-SRC_SUBDIR         = mono
+SRC_SUBDIR     = mono
 
-SOURCE_NAME        = mono
-SOURCE_SUFFIX      = tar.bz2
-SOURCE_VERSION     = $(VERSION)
-SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
-SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+SOURCE_NAME    = mono
+SOURCE_SUFFIX  = tar.bz2
+SOURCE_VERSION = $(VERSION)
+SOURCE_PKG     = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR     = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
 
-TAR_BZ2_PKGS       = $(SOURCE_PKG)
+TAR_BZ2_PKGS   = $(SOURCE_PKG)
 
-RPM.EXTRAS         = AutoReq:No
+RPM.EXTRAS     = AutoReq:No
